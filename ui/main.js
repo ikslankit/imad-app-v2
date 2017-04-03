7c970dfe53a0f0076f0379f43739d0bb619f6524
ikslankit <dagarankit90@gmail.com>
2017-04-03 14:55:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,13 +13,13 @@ button.onclick = function() {
     if ( request.readystate === XMLHttpRequest.DONE){
         if (request.status === 200) {
             var counter  = request.responseText;
+            var span = document.getElementById('count');    
         span.innerHTML = counter.toString();
             
         }
     }  
   };
   
-
   request.open('GET', 'http://ikslankit.imad.hasura-app.io/counter', true);
   request.send(null);
 };
