8e3b775fcc96db51b5f4fc29642b0048d7cb4b7d
ikslankit <dagarankit90@gmail.com>
2017-04-03 13:52:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,8 +4,11 @@ var button = document.getElementById('counter');
 button.onclick = function() {
   
   // Create a request object
+  
   var request = new XMLHttpRequest();
+  
   // Capture the response and store in it a varaible
+  
   request.onreadystatechange = function() {
     if ( request.readystate === XMLHttpRequest.DONE){
         if (request.status === 200) {
@@ -15,9 +18,8 @@ button.onclick = function() {
         }
     }  
   };
-  //Render the variable inn the correct span
-
   
-    request.open('GET', 'http://ikslankit.imad.hasura-app.io/counter', true);
-    request.send(null);
+
+  request.open('GET', 'http://ikslankit.imad.hasura-app.io/counter', true);
+  request.send(null);
 };
